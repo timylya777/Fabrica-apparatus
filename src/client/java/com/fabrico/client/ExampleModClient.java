1ef3091ca52b87231994a32e1432ec2cf3758eb1
timylya777 <timylya7773@gmail.com>
2026-07-28 16:04:26 +0300

    fixed 2 errors

--- src/client/java/com/fabrico/client/ExampleModClient.java
+++ src/client/java/com/fabrico/client/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example.client;
+package com.fabrico.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
--- src/client/java/com/fabrico/client/mixin/ExampleClientMixin.java
+++ src/client/java/com/fabrico/client/mixin/ExampleClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.client.mixin;
+package com.fabrico.client.mixin;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
