f323da0db6bbdae2ff8440b119197c5623a3de13
MrLOLchick <dzeki1366@gmail.com>
2026-07-28 20:20:59 +0500

    попробовал добавить блок press f

--- src/main/java/com/fabrica/FabricaMod.java
+++ src/main/java/com/fabrica/FabricaMod.java
@@ -2,6 +2,7 @@ package com.fabrica;
 
 import com.fabrica.item.ModItems; // для айтемов
 import com.fabrica.block.ModBlocks; // а тут догодайся с трех раз что значит modBlocks
+import com.fabrica.block.ModBlockItems; // блок айтем что мы держим
 
 import net.fabricmc.api.ModInitializer;
 
@@ -25,6 +26,7 @@ public class FabricaMod implements ModInitializer {
 		// Proceed with mild caution.
 		ModItems.register(); //регистрирую мод айтемы
 		ModBlocks.register(); // омагад блоки
+		ModBlockItems.register(); // регистрирую блок айтемы
 		LOGGER.info("We are alive!");
 	}
 
--- src/main/java/com/fabrica/block/ModBlocks.java
+++ src/main/java/com/fabrica/block/ModBlocks.java
@@ -1,9 +1,20 @@
 package com.fabrica.block;
 
-public class ModBlocks {
+import com.fabrica.FabricaMod;
 
-    public static void register() {
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
-    }
-
-}
+public static final Block MACHINE_CASING = Registry.register(
+        BuiltInRegistries.BLOCK,
+        ModBlockIds.MACHINE_CASING,
+        new Block(
+                BlockBehaviour.Properties.of()
+                        .setId(ModBlockIds.MACHINE_CASING)
+                        .strength(5.0F)
+                        .sound(SoundType.METAL)
+        )
+);
