cb9a645e92f83ab5fec0d5779511092e20da9a9e
MrLOLchick <dzeki1366@gmail.com>
2026-07-28 20:29:17 +0500

    добавляю тестовый блок machine casing

--- src/main/java/com/fabrica/block/ModBlockItems.java
+++ src/main/java/com/fabrica/block/ModBlockItems.java
@@ -1,17 +1,21 @@
 package com.fabrica.block;
 
-import com.fabrica.FabricaMod;
-
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 
-public static final Item MACHINE_CASING = Registry.register(
-        BuiltInRegistries.ITEM,
-        ModBlockIds.MACHINE_CASING_ITEM,
-        new BlockItem(
-                ModBlocks.MACHINE_CASING,
-                new Item.Properties().setId(ModBlockIds.MACHINE_CASING_ITEM)
-        )
-);
+public class ModBlockItems {
+
+    public static final Item MACHINE_CASING = Registry.register(
+            BuiltInRegistries.ITEM,
+            ModBlockIds.MACHINE_CASING_ITEM,
+            new BlockItem(
+                    ModBlocks.MACHINE_CASING,
+                    new Item.Properties().setId(ModBlockIds.MACHINE_CASING_ITEM)
+            )
+    );
+
+    public static void register() {
+    }
+}
--- src/main/java/com/fabrica/block/ModBlocks.java
+++ src/main/java/com/fabrica/block/ModBlocks.java
@@ -1,20 +1,24 @@
 package com.fabrica.block;
 
-import com.fabrica.FabricaMod;
-
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
-public static final Block MACHINE_CASING = Registry.register(
-        BuiltInRegistries.BLOCK,
-        ModBlockIds.MACHINE_CASING,
-        new Block(
-                BlockBehaviour.Properties.of()
-                        .setId(ModBlockIds.MACHINE_CASING)
-                        .strength(5.0F)
-                        .sound(SoundType.METAL)
-        )
-);
+public class ModBlocks {
+
+    public static final Block MACHINE_CASING = Registry.register(
+            BuiltInRegistries.BLOCK,
+            ModBlockIds.MACHINE_CASING,
+            new Block(
+                    BlockBehaviour.Properties.of()
+                            .setId(ModBlockIds.MACHINE_CASING)
+                            .strength(5.0F)
+                            .sound(SoundType.METAL)
+            )
+    );
+
+    public static void register() {
+    }
+}
