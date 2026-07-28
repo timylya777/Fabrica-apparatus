9b9b762e6b4b1f2ef581dfd1878cd31eb3396039
MrLOLchick <dzeki1366@gmail.com>
2026-07-28 19:13:26 +0500

    что-то фиксил, если что тима поможет

--- src/main/java/com/fabrica/item/ModItems.java
+++ src/main/java/com/fabrica/item/ModItems.java
@@ -1,7 +1,19 @@
 package com.fabrica.item;
 
-public static final Item IRON_FIGURE = Registry.register(
-    BuiltInRegistries.ITEM,
-    FabricaMod.id("iron_figure"),
-    new Item(new Item.Properties())
-);
+import com.fabrica.FabricaMod;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
+
+public class ModItems {
+
+    public static final Item IRON_FIGURE = Registry.register(
+        BuiltInRegistries.ITEM,
+        FabricaMod.id("iron_figure"),
+        new Item(new Item.Properties())
+    );
+
+    public static void register() {
+    }
+}
