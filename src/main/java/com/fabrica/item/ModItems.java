8dd65ad4bc159c5e0725bfb97b4fb7b5bf3a7a21
MrLOLchick <dzeki1366@gmail.com>
2026-07-28 19:09:30 +0500

    наконец добавил предмет как таковой

--- src/main/java/com/fabrica/item/ModItems.java
+++ src/main/java/com/fabrica/item/ModItems.java
@@ -1,9 +1,7 @@
 package com.fabrica.item;
 
-public class ModItems {
-
-    public static void register() {
-
-    }
-
-}
+public static final Item IRON_FIGURE = Registry.register(
+    BuiltInRegistries.ITEM,
+    FabricaMod.id("iron_figure"),
+    new Item(new Item.Properties())
+);
