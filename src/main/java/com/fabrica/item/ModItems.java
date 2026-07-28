f7a147847ed83f52ba19c303fb1234cc7cc5887b
MrLOLchick <dzeki1366@gmail.com>
2026-07-28 19:23:31 +0500

    Я дурашка создавал предмет по старому гайду а надо 26.2

--- src/main/java/com/fabrica/item/ModItems.java
+++ src/main/java/com/fabrica/item/ModItems.java
@@ -1,19 +1,35 @@
 package com.fabrica.item;
 
-import com.fabrica.FabricaMod;
+import java.util.function.Function;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
 public class ModItems {
 
-    public static final Item IRON_FIGURE = Registry.register(
-        BuiltInRegistries.ITEM,
-        FabricaMod.id("iron_figure"),
-        new Item(new Item.Properties())
-    );
+    public static <T extends Item> T register(
+            net.minecraft.resources.ResourceKey<Item> key,
+            Function<Item.Properties, T> factory,
+            Item.Properties properties
+    ) {
+
+        T item = factory.apply(properties.setId(key));
+
+        Registry.register(BuiltInRegistries.ITEM, key, item);
+
+        return item;
+    }
+
+    public static final Item IRON_FIGURE =
+            register(ModItemIds.IRON_FIGURE, Item::new, new Item.Properties());
 
     public static void register() {
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS)
+                .register(entries -> entries.accept(IRON_FIGURE));
+
     }
 }
