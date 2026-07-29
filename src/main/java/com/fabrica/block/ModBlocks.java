151611dd61f3f8d5129d63bd939587ec88b3f297
timylya777 <timylya7773@gmail.com>
2026-07-29 21:27:40 +0300

    add creative tab to blocks

--- src/main/java/com/fabrica/block/ModBlocks.java
+++ src/main/java/com/fabrica/block/ModBlocks.java
@@ -7,16 +7,22 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 
 public class ModBlocks {
 
 
     // BLOCK REGISTERGING IN ONE METHOD
-    private static Block register(String id, BlockBehaviour.Properties properties) {
+    private static Block register(
+        String id,
+        BlockBehaviour.Properties properties,
+        ResourceKey<CreativeModeTab> creativeTab
+) {
     Identifier identifier = Identifier.fromNamespaceAndPath(FabricaMod.MOD_ID, id);
 
     ResourceKey<Block> blockKey = ResourceKey.create(Registries.BLOCK, identifier);
@@ -26,20 +32,28 @@ public class ModBlocks {
 
     Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
 
-    Registry.register(
+    Item blockItem = Registry.register(
             BuiltInRegistries.ITEM,
             itemKey,
             new BlockItem(block, new Item.Properties().setId(itemKey))
     );
 
+    CreativeModeTabEvents.modifyOutputEvent(creativeTab)
+            .register(output -> output.accept(blockItem));
+
     return block;
     }
+
 
     public static final Block MACHINE_CASING = register(
         "machine_casing",
         BlockBehaviour.Properties.of()
                 .strength(5.0F)
-                .sound(SoundType.METAL)
+                .sound(SoundType.METAL),
+        ResourceKey.create(
+                Registries.CREATIVE_MODE_TAB,
+                Identifier.withDefaultNamespace("ingredients")
+        )
     );
 
     public static void register() {
