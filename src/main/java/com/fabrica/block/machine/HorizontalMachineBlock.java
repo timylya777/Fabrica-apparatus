a9929726c69d4bdde5941fcb542f97e1f1031d43
MrLOLchick <dzeki1366@gmail.com>
2026-07-30 21:15:08 +0500

    какие-то минор ченджес

--- src/main/java/com/fabrica/block/machine/HorizontalMachineBlock.java
+++ src/main/java/com/fabrica/block/machine/HorizontalMachineBlock.java
@@ -2,13 +2,30 @@ package com.fabrica.block.machine;
 
 import com.fabrica.api.energy.IEnergyConnectable;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 public abstract class HorizontalMachineBlock extends HorizontalDirectionalBlock implements IEnergyConnectable {
+    public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
+
     public HorizontalMachineBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
